985c00d092bda5c4872e88bd654033ea3be7a3bf
90E-Mang <ggot2000@naver.com>
2022-03-22 19:59:27 +0900

    백준 10926

---
+++ Backjoon10926/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backjoon10926
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] id = Console.ReadLine().Split();
+
+            if (id.Contains(id[0]))
+            {
+                Console.WriteLine(id[0] + "??!");
+            }
+        }
+    }
+}
